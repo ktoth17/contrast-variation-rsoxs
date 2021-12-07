6f29b90986abf41bda26efd77cedea0cfc2d87d7
Kristof Toth <kristoft55@gmail.com>
2021-12-07 17:44:17 -0500

    Indentation fix

--- cvworkflow/kkcalcfunctions.py
+++ cvworkflow/kkcalcfunctions.py
@@ -13,43 +13,43 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 def kkcalc_convert(file_path, *, chemical_formula, density, min_ev, max_ev, load_options=None,input_data_type='Beta',add_background=False,fix_distortions=False,curve_tolerance=None,curve_recursion=50):
-'''
-This file is part of the Kramers-Kronig Calculator software package.
-Copyright (c) 2013 Benjamin Watts, Daniel J. Lauk
-The software is licensed under the terms of the zlib/libpng license.
-For details see LICENSE.txt
+    """
+    This file is part of the Kramers-Kronig Calculator software package.
+    Copyright (c) 2013 Benjamin Watts, Daniel J. Lauk
+    The software is licensed under the terms of the zlib/libpng license.
+    For details see LICENSE.txt
 
-Benjamin Watts "Calculation of the Kramers-Kronig transform of X-ray spectra by a piecewise Laurent polynomial method" Opt. Express 22, (2014) 23628-23639. DOI:10.1364/OE.22.023628
+    Benjamin Watts "Calculation of the Kramers-Kronig transform of X-ray spectra by a piecewise Laurent polynomial method" Opt. Express 22, (2014) 23628-23639. DOI:10.1364/OE.22.023628
 
-Pip page for kkcalc: https://pypi.org/project/kkcalc/
+    Pip page for kkcalc: https://pypi.org/project/kkcalc/
 
-Benjamin Watts github: https://github.com/benajamin/kkcalc
+    Benjamin Watts github: https://github.com/benajamin/kkcalc
 
-Cloned repository from Daniel Schick with an easy to follow along example: https://github.com/dschick/kkcalc
+    Cloned repository from Daniel Schick with an easy to follow along example: https://github.com/dschick/kkcalc
 
-Parameters
-----------
-file_path : pathlib.WindowsPath
-    Incident angle in radians.
-chemical_formula : string
-    The chemical formula of the component, i.e. 'C8H8' for polystyrene.
-density : float
-    Density of the component in grams/cc. Typically around 1 grams/cc.
+    Parameters
+    ----------
+    file_path : pathlib.WindowsPath
+        Incident angle in radians.
+    chemical_formula : string
+        The chemical formula of the component, i.e. 'C8H8' for polystyrene.
+    density : float
+        Density of the component in grams/cc. Typically around 1 grams/cc.
 
-Returns
--------
-delta : numpy.ndarray
-    Real (dispersive) components of the complex index of refraction. Two columns, energy and delta values.
-beta : numpy.ndarray
-    Imaginary (absorptive) components of the complex index of refraction. Two columns, energy and beta values.
+    Returns
+    -------
+    delta : numpy.ndarray
+        Real (dispersive) components of the complex index of refraction. Two columns, energy and delta values.
+    beta : numpy.ndarray
+        Imaginary (absorptive) components of the complex index of refraction. Two columns, energy and beta values.
 
-Examples
---------
-Calculate the complex index of refraction of polystrene (PS) from the NEXAFS of PS given from a txt file.
+    Examples
+    --------
+    Calculate the complex index of refraction of polystrene (PS) from the NEXAFS of PS given from a txt file.
 
->>> kkcalc_convert(file_path, *, chemical_formula= 'C8H8', density = 1.05, min_ev=270, max_ev=325, )
+    >>> kkcalc_convert(file_path, *, chemical_formula= 'C8H8', density = 1.05, min_ev=270, max_ev=325, )
 
-'''
+    """
 
     merge_points=[min_ev, max_ev]
 

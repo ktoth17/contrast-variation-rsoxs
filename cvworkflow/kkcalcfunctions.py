93ed6afe80321b4a80e7dccdf738bab1916d8e6e
Kristof Toth <kristoft55@gmail.com>
2021-12-28 14:57:47 -0500

    make_contrast_M_2 bug fix

--- cvworkflow/kkcalcfunctions.py
+++ cvworkflow/kkcalcfunctions.py
@@ -168,7 +168,6 @@ def make_contrast_M_2(delta1, beta1, label1, delta2, beta2, label2, new_q_index)
         index_df = pd.DataFrame(delta1_df, columns=[delta_label1])
         index_df.insert(1, delta_label2, delta2_df, True)
         index_df.insert(2, beta_label1, beta1_df, True)
-        index_df.insert(3, beta_label2, beta2_df, True)
 
 
         contrast_df = index_df.copy(deep=True)
